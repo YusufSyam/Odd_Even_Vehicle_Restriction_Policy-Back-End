57d6036bd5d2698a7657f3c6e8510d770574c6dd
YusufSyam <muhyusufsyamsyam@gmail.com>
2024-02-07 22:27:42 +0800

    Init konfig untuk fungsi deteksi

--- app/detect/detect.py
+++ app/detect/detect.py
@@ -3,6 +3,139 @@ from app.utils.functions.file import delete_image_if_exists, decode_and_save_ima
 
 from app.utils.const.directory import DETECTION_IMAGE_FOLDER, TEMPORARY_IMAGE_FOLDER, DETECTION_IMAGE_FOLDER, PLATE_IMAGE_FOLDER, FRAME_IMAGE_FOLDER
 
+from app.detect.detect_const import *
+
+from ultralytics import YOLO
+import cv2
+import time
+import torch
+
+
+
+coco_model = YOLO(COCO_MODEL)
+license_plate_detector = YOLO(LICENSE_PLATE_MODEL_PATH)
+
+def oevlpr_detection(frame, frame_num=0, count_runtime=False, filename= '', upscale_amount= 6, vertical_stretch= 0.6, opening_kernel_size=(3,3), apply_remove_noise= False, plot= True):
+    global coco_model, license_plate_detector
+    global TEMP_CAR_IMG_DIR, TEMP_FRAME_IMG_DIR, TEMP_LICENSE_PLATE_IMG_DIR
+
+    # Hapuski ini kalau production mi
+    start_time= time.time()
+
+    detections = coco_model(frame, conf= 0.01)[0]
+    detected_car = []
+    for detection in detections.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = detection
+        if int(class_id) == 2:
+            detected_car.append([x1, y1, x2, y2, score])
+
+    # print('detected_car', detected_car)
+
+    # Tidak ada mobil yang terdeteksi
+    # if len(detected_car)<= 0:
+    #     track_ids = []
+    # else:
+    #     track_ids = mot_tracker.update(np.asarray(detected_car))
+    
+    track_ids = []
+    license_plates = license_plate_detector(frame)[0]
+
+    identifier= filename.split('.')[0]
+
+    is_frame_img_made= False
+    for license_plate in license_plates.boxes.data.tolist():
+        x1, y1, x2, y2, score, class_id = license_plate
+        print('IDENTIFIER:', identifier)
+
+        xcar1, ycar1, xcar2, ycar2, car_id = get_car(license_plate, track_ids)
+        xcar1, ycar1, xcar2, ycar2 = limit_at_zero(xcar1), limit_at_zero(ycar1), limit_at_zero(xcar2), limit_at_zero(ycar2)
+
+        if True:
+        # if car_id != -1:
+
+            # crop car & license plate
+            car_crop = frame[int(ycar1):int(ycar2), int(xcar1): int(xcar2), :]
+            license_plate_crop = frame[int(y1):int(y2), int(x1): int(x2), :]
+
+            # process license plate
+            license_plate_crop_gray = cv2.cvtColor(license_plate_crop, cv2.COLOR_BGR2GRAY)
+
+            license_plate_crop_straight = straightening_image(license_plate_crop_gray)
+            license_plate_crop_straight = upscale_image(license_plate_crop_straight, upscale_amount)
+
+            if apply_remove_noise:
+                license_plate_crop_straight= remove_noise(license_plate_crop_straight)
+
+            license_plate_crop_thresh = thresholding1(license_plate_crop_straight)
+            license_plate_crop_thresh= opening(license_plate_crop_thresh, opening_kernel_size)
+            license_plate_crop_thresh= cv2.bitwise_not(license_plate_crop_thresh)
+            license_plate_crop_thresh= stretch_vertical(license_plate_crop_thresh, vertical_stretch)
+            
+            if plot:
+              plt.title('license_plate_crop_gray')
+              plt.imshow(license_plate_crop_gray, cmap='gray')
+              plt.show()
+
+              plt.title('license_plate_crop_thresh')
+              plt.imshow(license_plate_crop_thresh, cmap='gray')
+              plt.show()
+              
+            # read license plate number
+            license_plate_text, license_plate_text_score = read_license_plate(license_plate_crop_thresh)
+
+            if True:
+            # if license_plate_text is not None:
+                if not is_frame_img_made:
+                    frame_image_filename = os.path.join(TEMP_FRAME_IMG_DIR, f"frame:{frame_num}-identifier:{identifier}.png")
+                    cv2.imwrite(frame_image_filename, frame)
+
+                    is_frame_img_made= True
+
+                    if car_id==-1:
+                      car_image_filename= None
+                    else:
+                      car_image_filename = os.path.join(TEMP_CAR_IMG_DIR, f"frame:{frame_num}-car_id:{car_id}-identifier:{identifier}.png")
+                      
+                try:
+                    cv2.imwrite(car_image_filename, car_crop)
+                except:
+                    print('Car not found')
+
+                license_plate_image_filename = os.path.join(TEMP_LICENSE_PLATE_IMG_DIR,
+                            f"frame:{frame_num}-car_id:{car_id}-license_plate-{license_plate_text}-{identifier}.png")
+        
+                cv2.imwrite(license_plate_image_filename, license_plate_crop)
+
+                license_plate_num= get_license_plate_num(license_plate_text)
+                try:
+                    is_car_violating= get_is_car_violating(int(license_plate_num))
+                except:
+                    is_car_violating= None
+
+                temp_result_dict={'raw_license_plate_text':license_plate_text,
+                                  'car_img_filename':car_image_filename, 'frame_img_filename':frame_image_filename,
+                                  'license_plate_img_filename':license_plate_image_filename,
+
+                                  }
+                
+                return temp_result_dict
+
+    if count_runtime:
+        end_time = time.time()
+        print(f'Running Time per Frame: {end_time-start_time}')
+
+    return None
+
+
+def detect_plate_on_sent_image(image):
+  result_dict= oevlpr_detection(image, mot_tracker, filename=temp_img_path)
+
+  print('result_dict:',result_dict)
+
+
+
+
+
 def get_dummy_detection(image_data, image_name):
     save_image_cv(image_data, image_name, DETECTION_IMAGE_FOLDER)
     save_image_cv(image_data, image_name, PLATE_IMAGE_FOLDER)
---
+++ app/detect/helper/image_preprocess.py
@@ -0,0 +1,132 @@
+import cv2
+import numpy as np
+
+def straightening_image(image):
+    # Temukan tepi gambar
+    edges = cv2.Canny(image, 50, 150, apertureSize=3)
+
+    # Temukan garis-garis menggunakan transformasi Hough
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, 100)
+    # plt.imshow(edges)
+    # print('/nLines/n', lines)
+
+    if lines is None:
+        print('NOT STRAIGHTENED')
+        return image
+
+    # Temukan sudut rotasi
+    for line in lines:
+        rho, theta = line[0]
+        if np.degrees(theta) > 45 and np.degrees(theta) < 135:
+            rotation_angle = np.degrees(theta) - 90
+            break
+    else:
+        print('NOT STRAIGHTENED')
+        return image
+
+    # Rotasi gambar
+    rows, cols = image.shape
+    M = cv2.getRotationMatrix2D((cols / 2, rows / 2), rotation_angle, 1)
+    rotated_img = cv2.warpAffine(image, M, (cols, rows))
+
+    return rotated_img
+
+def stretch_vertical(img, height_multiply= 1.5):
+    # Dapatkan dimensi asli gambar
+    height= img.shape[0]
+    width = img.shape[1]
+    # height, width, _ = img.shape
+
+    # Lakukan pengstretchan secara vertikal
+    stretched_height = int(height * height_multiply)
+    stretched_img = cv2.resize(img, (width, stretched_height))
+
+    # Tampilkan gambar menggunakan pyplot
+    # plt.imshow(cv2.cvtColor(stretched_img, cv2.COLOR_BGR2RGB))
+    # plt.title('Gambar Stretched Vertikal')
+    # plt.axis('off')
+    # plt.show()
+    return stretched_img
+
+def upscale_image(img, amount= 10):
+    # Dapatkan dimensi asli gambar
+    height, width = img.shape
+
+    # Upscale gambar sebanyak 2x panjang dan lebar
+    new_height = height *amount
+    new_width = width *amount
+    upscaled_img = cv2.resize(img, (new_width, new_height))
+
+    # Simpan gambar yang sudah diupscale
+    # cv2.imwrite(output_path, upscaled_img)
+
+    return upscaled_img
+
+def preprocess_plate(image, show= False):
+    th, threshed = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # threshed = thresholding3(image)
+    final_img= cv2.bitwise_not(threshed)
+
+    if show:
+        plt.imshow(final_img, cmap='binary')
+        plt.show()
+
+    return final_img
+
+# get grayscale image
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# noise removal
+def remove_noise(image):
+    return cv2.GaussianBlur(image, (3, 3), 0)
+
+#thresholding1
+def thresholding1(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+#thresholding2
+def thresholding2(image):
+    _, thresh = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY_INV)
+    return thresh
+
+#thresholding3
+def thresholding3(image):
+    return cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 45, 15)
+
+#dilation
+def dilate(image):
+    kernel = np.ones((3,3),np.uint8)
+    return cv2.dilate(image, kernel, iterations = 1)
+
+#erosion
+def erode(image):
+    kernel = np.ones((3,3),np.uint8)
+    return cv2.erode(image, kernel, iterations = 1)
+
+#opening - erosion followed by dilation
+def opening(image, kernel_size= (3,3)):
+    kernel = np.ones(kernel_size,np.uint8)
+    return cv2.morphologyEx(image, cv2.MORPH_OPEN, kernel)
+
+#canny edge detection
+def canny(image):
+    return cv2.Canny(image, 100, 200)
+
+#skew correction
+def deskew(image):
+    coords = np.column_stack(np.where(image > 0))
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(9 + angle)
+    else:
+        angle = -angle
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+    return rotated
+
+#template matching
+def match_template(image, template):
+    return cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
